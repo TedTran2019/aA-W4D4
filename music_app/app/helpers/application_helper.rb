397cee02023be655b559ed01f8697459731ac243
Ted Tran <TedTran2019@gmail.com>
2019-10-10 00:54:20 -0700

    Completed phase 3 making the lyric helper

--- music_app/app/helpers/application_helper.rb
+++ music_app/app/helpers/application_helper.rb
@@ -5,4 +5,14 @@ module ApplicationHelper
 		value=\"#{form_authenticity_token}\"/
 		>".html_safe
 	end
+
+	def ugly_lyrics(lyrics)
+		split_lyrics = lyrics.split("\n")
+		beautified = ""
+		split_lyrics.each do |line|
+			beautified << "&#9835 "
+			beautified << "#{h(line)}\n"
+		end
+		"<pre>#{beautified}</pre>".html_safe
+	end
 end
